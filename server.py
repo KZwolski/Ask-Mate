8e4ae8c93b6e8c445682d5cb5de682ea3c3cb64d
KZwolski <kkzwolski@gmail.com>
2022-05-19 10:28:11 +0200

    small changes

--- server.py
+++ server.py
@@ -172,7 +172,7 @@ def users_list():
         return render_template("users.html", users_details=users_details)
     else:
         user_logout = True
-        return render_template('login.html', user_logout=user_logout)
+        return render_template('users.html', user_logout=user_logout)
 
 
 
